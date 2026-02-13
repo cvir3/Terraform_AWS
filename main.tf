49b58606a5ffe313ded30ba1b4000164bf421fec
cvir3 <vir3.chauhan@gmail.com>
2026-02-13 18:58:43 +0530

    create variable file.
    create variable file and used in the main file.

--- main.tf
+++ main.tf
@@ -8,15 +8,16 @@ terraform {
 }
 
 provider "aws" {
-    region = "ap-south-1"  
+    # Here used the variable
+    region = var.region  
 }
 
 resource "aws_instance" "myserver" {
-    // AMI ID identifies the OS image and its configuration used for EC2. 
-    ami = "ami-019715e0d74f695be" 
+    # AMI ID identifies the OS image and its configuration used for EC2. 
+    ami = "ami-0317b0f0a0144b137" 
     instance_type = "t3.micro"  
 
     tags = {
-        Name = "SampleServer"
+        Name = "V_SampleServer"
     }
 }
