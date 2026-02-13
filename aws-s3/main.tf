c3cb2ac12cc65707a903179f5b8c07a77c05fa3e
cvir3 <vir3.chauhan@gmail.com>
2026-02-13 22:41:48 +0530

    use random provider
    It is create random string or id
    https://registry.terraform.io/providers/hashicorp/random/latest

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -4,6 +4,10 @@ terraform {
         source = "hashicorp/aws"
         version = "6.32.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.8.1"
+    }
   }
 }
 
@@ -11,14 +15,35 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
+resource "random_id" "rando_id" {
+    byte_length = 8
+  
+}
+
+resource "aws_s3_bucket" "test-demo" {
+    # test-demo is configuration block name
+    bucket = "first-s3-buckets-${random_id.rando_id.hex}"
+    # this is first-s3-bucket-2026 bucket name     
+}
+
+output "uniqueId" {
+    value = random_id.rando_id
+  
+}
+
+/* This is for create s3 bucket
 resource "aws_s3_bucket" "test-demo" {
     # test-demo is configuration block name
     bucket = "first-s3-bucket-2026"
     # this is first-s3-bucket-2026 bucket name     
-}
+} */
 
+
+/* This is for upload file
 resource "aws_s3_object" "bucket-data" {
     bucket = aws_s3_bucket.test-demo.bucket
     source = "./myfile.txt"
     key = "mydata.txt"  
 }
+*/
+
